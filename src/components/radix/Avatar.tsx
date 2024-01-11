f2bac58411f105aaf45da7eca0508825fa4d2dd0
lecto17 <khwoo_rpsit@naver.com>
2024-01-11 20:15:54 +0900

    feat: 프로필 이미지 클릭 시 프로필 이미지 리스트 출력될 수 있도록 작업 중

--- src/components/radix/Avatar.tsx
+++ src/components/radix/Avatar.tsx
@@ -1,27 +1,47 @@
-import * as AvatarPrimitive from '@radix-ui/react-avatar';
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { useCallback, useState } from "react";
 
 const AvatarDemo = ({
-	src,
-	alt,
-	initials,
+  src,
+  alt,
+  initials,
 }: {
-	src: string;
-	alt: string;
-	initials: string;
-}) => (
-	<AvatarPrimitive.Root className="AvatarRoot inline-flex items-center justify-center overflow-hidden w-12 h-12 rounded-full bg-slate-900">
-		<AvatarPrimitive.Image
-			className="AvatarImage w-100 h-100 object-cover"
-			src={src}
-			alt={alt}
-		/>
-		<AvatarPrimitive.Fallback
-			className="AvatarFallback w-100 h-100 flex items-center justify-center text-base text-white leading-none font-semibold"
-			delayMs={600}
-		>
-			{initials}
-		</AvatarPrimitive.Fallback>
-	</AvatarPrimitive.Root>
-);
+  src: string;
+  alt: string;
+  initials: string;
+}) => {
+  const [profileImage, setProfileImage] = useState(src || "");
+  const [showable, setShowable] = useState(false);
+
+  const chooseProfileImages = useCallback(() => {
+    if (!showable) {
+    }
+
+    setShowable((prev) => !prev);
+  }, [showable]);
+
+  return (
+    <AvatarPrimitive.Root className="AvatarRoot inline-flex items-center justify-center overflow-hidden w-12 h-12 rounded-full bg-slate-900">
+      <div className="AvartarImage-wrapper relative">
+        <AvatarPrimitive.Image
+          className="AvatarImage w-1000 h-100 object-cover cursor-pointer"
+          src={profileImage || src}
+          alt={alt}
+          onClick={chooseProfileImages}
+        />
+        {showable && (
+          <div className="absolute top-0 w-20 h-50 bg-red-300 z-50 ">ddd</div>
+        )}
+      </div>
+
+      <AvatarPrimitive.Fallback
+        className="AvatarFallback w-100 h-100 flex items-center justify-center text-base text-white leading-none font-semibold"
+        delayMs={600}
+      >
+        {initials}
+      </AvatarPrimitive.Fallback>
+    </AvatarPrimitive.Root>
+  );
+};
 
 export default AvatarDemo;
